a416a43eddb0b0cb2c76350ff5f8da2ce2b543a9
SomeoneG <gashatalenta2@gmail.com>
2025-06-22 22:48:57 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Happy Birthday Nazma🙌').split('')
+    const titles = ('Happy Birthday Nazma🤍').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
